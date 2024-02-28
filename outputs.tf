681d701d87967d745ee1f3d2add825a26594d564
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-02-29 00:23:58 +0100

    feat: add `dashboard` stack to outputs

--- outputs.tf
+++ outputs.tf
@@ -44,3 +44,15 @@ output "library" {
     containers = [docker_container.calibre_web.name, docker_container.audiobookshelf.name, docker_container.library_tunnel.name]
   }
 }
+
+output "dashboard" {
+  value = {
+    tunnel = {
+      id    = cloudflare_tunnel.dashboard_tunnel.id
+      name  = cloudflare_tunnel.dashboard_tunnel.name
+      cname = cloudflare_tunnel.dashboard_tunnel.cname
+    }
+    cname      = [cloudflare_record.homepage.hostname]
+    containers = [docker_container.homepage.name, docker_container.dashboard_tunnel.name]
+  }
+}
