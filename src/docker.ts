e32d3b15cfe59332615f449fd4b46d0d7be1cac3
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-04-26 18:46:20 +0000

    fix: add missing `pulumi.ComponentResourceOptions` to `LocalStack`

--- src/docker.ts
+++ src/docker.ts
@@ -1,15 +1,20 @@
 import * as docker from "@pulumi/docker";
+import type * as pulumi from "@pulumi/pulumi";
 
 export class LocalVolume extends docker.Volume {
-  constructor(name: string, device: string) {
-    super(name, {
+  constructor(name: string, device: string, opts?: pulumi.ComponentResourceOptions) {
+    super(
       name,
-      driver: "local",
-      driverOpts: {
-        device,
-        type: "none",
-        o: "bind",
+      {
+        name,
+        driver: "local",
+        driverOpts: {
+          device,
+          type: "none",
+          o: "bind",
+        },
       },
-    });
+      opts,
+    );
   }
 }
