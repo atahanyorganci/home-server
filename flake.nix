4db0ae6e5958e686f313039af97fc58c9effacd8
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-22 23:44:36 +0200

    feat: replace `.mise.toml` with Nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix flake for managing homelab infrastructure with Terraform";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+  outputs = { nixpkgs, ... }:
+    let
+      systems = nixpkgs.lib.platforms.all;
+      eachSystem = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+            config = {
+              allowUnfree = true;
+            };
+          };
+        in f pkgs
+      );
+    in
+    {
+      formatter = eachSystem (pkgs: pkgs.nixpkgs-fmt);
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            terraform
+          ];
+        };
+      });
+    };
+}
